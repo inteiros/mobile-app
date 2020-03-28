890477d617f157d37bdd87ca89ce294f68c518f0
JoaoEler <jgelermendes@gmail.com>
2020-03-28 19:32:51 -0300

    mobile

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Eler!</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
